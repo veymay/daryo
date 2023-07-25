14905ab96262fa588ade79870c1a3b1a901b9c74
VEYMAY <veymayuz@gmail.com>
2023-07-25 14:49:19 +0500

    add js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let borderColors = ['#6cf', '#c9f', '#9ff', '#f90', '#ff0', '#c9f', '#f69']
+let menuItem = document.querySelectorAll('ul.menu>li>a')
+console.log(menuItem);
+
+menuItem.forEach((item,i) => {
+  item.style.borderColor = borderColors[i]
+  item.addEventListener('mouseenter', () => {
+    item.style.color = borderColors[i]
+  })
+  item.addEventListener('mouseleave', () => {
+    item.style.color = 'inherit'
+  })
+})
