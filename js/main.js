44cabbcc5ea6013f832d043029e13777ca91d43d
VEYMAY <veymayuz@gmail.com>
2023-07-29 15:05:04 +0500

    123332323548

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let menu = document.querySelector('.menu')
 let search = document.querySelector('.search')
-let searchInp = document.querySelector('.search_input')
 let searchBtn = document.querySelector('.search_button')
 let closeSearch = document.querySelector('.close_search')
 let searchOverlay = document.querySelector('.search_overlay')
@@ -8,12 +7,29 @@ let uzb = ['MAHALLIY', 'MARKAZIY OSIYO', 'DUNYO', 'PUL', 'MADANIYAT', 'LAYFSTAYL
 let uzbKril = ['МАҲАЛЛИЙ', 'МАРКАЗИЙ ОСИЙЎ', 'ДУНЙЎ', 'ПУЛ', 'МАДАНИЯТ', 'ЛАЙФСТАЙЛ', 'СПОРТ', 'КОЛУМНИСТЛАР', 'МУЛЬТИМEДИА']
 let rus = ['МЕСТНЫЕ', 'ЦЕНТРАЛЬНАЯ АЗИЯ', 'МИР', 'ДЕНЬГИ', 'КУЛЬТУРА', 'ОБРАЗ ЖИЗНИ', 'СПОРТ', 'ОБОЗНАЧИТЕЛИ', 'МУЛЬТИМЕДИА']
 let eng = ['LOCAL', 'CENTRAL ASIA', 'WORLD', 'MONEY', 'CULTURE', 'LIFESTYLE', 'SPORTS', 'COLUMNISTS', 'MULTIMEDIA']
-let menuItem = document.querySelectorAll('ul.menu>li>a')
+let borderColors = ['#6cf', '#c9f', '#9ff', '#f90', '#ff0', '#c9f', '#f69', '#6cf', '#c9f', '#9ff', '#f90', '#ff0',]
 let langActive = document.querySelector('.active_lang')
 let langs = document.querySelector('ul.language')
 let langsItem = document.querySelectorAll('ul.language>li')
+
+function menuItemColor() {
+  let menuItem = document.querySelectorAll('ul.menu>li>a')
+  menuItem.forEach((item,i) => {
+    item.style.borderColor = borderColors[i]
+    item.addEventListener('mouseenter', () => {
+      item.style.color = borderColors[i]
+    })
+    item.addEventListener('mouseleave', () => {
+      item.style.color = 'inherit'
+    })
+  })
+} 
+
+menuItemColor()
+
 langActive.addEventListener('click', () => {
   langs.classList.toggle('active')
+  
   langsItem.forEach(langItem => {
     langItem.addEventListener('click', () => {
       langItem.classList.add('active')
@@ -22,24 +38,28 @@ langActive.addEventListener('click', () => {
         menu.innerHTML=''
         uzb.forEach(uz => {
           menu.innerHTML += `<li><a href='#'>${uz}</a></li>`
+          menuItemColor()
         })
       } else if (langItem.innerHTML == 'Ўзбекча') {
         langActive.innerHTML = 'Ўзб'
         menu.innerHTML=''
         uzbKril.forEach(uzkril => {
           menu.innerHTML += `<li><a href='#'>${uzkril}</a></li>`
+          menuItemColor()
         })
       } else if (langItem.innerHTML == 'Русский') {
         langActive.innerHTML = 'РУС'
         menu.innerHTML=''
         rus.forEach(ru => {
           menu.innerHTML += `<li><a href='#'>${ru}</a></li>`
+          menuItemColor()
         })
       } else {
         langActive.innerHTML = 'ENG'
         menu.innerHTML=''
         eng.forEach(en => {
           menu.innerHTML += `<li><a href='#'>${en}</a></li>`
+          menuItemColor()
         })
       }
       langsItem.forEach(langitem2 => {
@@ -51,34 +71,20 @@ langActive.addEventListener('click', () => {
   })
 })
 
-let borderColors = ['#6cf', '#c9f', '#9ff', '#f90', '#ff0', '#c9f', '#f69', '#6cf', '#c9f', '#9ff', '#f90', '#ff0',]
-
-menuItem.forEach((item,i) => {
-  item.style.borderColor = borderColors[i]
-  item.addEventListener('mouseenter', () => {
-    item.style.color = borderColors[i]
-  })
-  item.addEventListener('mouseleave', () => {
-    item.style.color = 'inherit'
-  })
-})
-
 // Search
+let searchInp = document.querySelector('.search_input')
+
 search.addEventListener('click', () => {
   document.body.style.padding='0 16px 0 0'
   document.body.style.overflow='hidden'
   searchOverlay.classList.toggle('active')
-  searchInp.focus()
 })
-searchBtn.style.opacity='0'
-// searchInp.value=''
-document.addEventListener('keydown', () => {
-  console.log(searchInp.value,"-",searchInp.value.length);
-  searchInp.focus()
-  if (searchInp.value.length>=0) {
-    searchBtn.style.opacity='1'
+searchBtn.style.opacity = '0'
+searchInp.addEventListener('keyup', () => {
+  if (searchInp.value.length==0) {
+    searchBtn.style.opacity = '0'
   } else {
-    searchBtn.style.opacity='0'
+    searchBtn.style.opacity = '1'
   }
 })
 closeSearch.addEventListener('click', () => {
@@ -86,3 +92,18 @@ closeSearch.addEventListener('click', () => {
   document.body.style.padding='0 0px 0 0'
   searchOverlay.classList.toggle('active')
 })
+
+
+
+
+document.addEventListener('keyup', (e) => {
+  if (e.key=='Enter') {
+    function getSelectedText() {
+      let selectedText = window.getSelection()
+      console.log(selectedText);
+      console.log(selectedText.createRange().data);
+    }
+    getSelectedText()
+  }
+})
+
